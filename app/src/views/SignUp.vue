8e215d8c0a3c0ebbb223c741d71600e55ee1c5a6
JackF710 <jtfeery@gmail.com>
2025-05-09 14:00:14 -0400

    k

--- app/src/views/SignUp.vue
+++ app/src/views/SignUp.vue
@@ -7,6 +7,8 @@
       <input type="password" v-model="password" placeholder="Password" required />
       <button type="submit">Sign Up</button>
     </form>
+    <p v-if="errorMsg">{{ errorMsg }}</p>
+    <p v-if="infoMsg">{{ infoMsg }}</p>
   </div>
 </template>
 
@@ -19,25 +21,44 @@ export default {
       email: '',
       username: '',
       password: '',
+      errorMsg: '',
+      infoMsg: '',
     }
   },
   methods: {
     async signUp() {
-      const { user, error } = await supabase.auth.signUp({
+      this.errorMsg = ''
+      this.infoMsg = ''
+
+      const { data, error } = await supabase.auth.signUp({
         email: this.email,
         password: this.password,
       })
 
       if (error) {
-        console.error('Error signing up:', error.message)
-      } else {
-        const { data, error: profileError } = await supabase.from('profiles')
+        this.errorMsg = 'Error signing up: ' + error.message
+        return
+      }
 
-        if (profileError) {
-          console.error('Error creating profile:', profileError.message)
-        } else {
-          this.$router.push('/auth') // Redirect to profile page
-        }
+      const userId = data?.user?.id
+
+      if (!userId) {
+        this.infoMsg = 'Please confirm your email before continuing.'
+        return
+      }
+
+      const { error: profileError } = await supabase.from('profiles').insert([
+        {
+          id: userId,
+          username: this.username,
+        },
+      ])
+
+      if (profileError) {
+        this.errorMsg = 'Error saving profile: ' + profileError.message
+      } else {
+        this.infoMsg = 'Account created! You can now log in.'
+        this.$router.push('/login') // or wherever you want to go
       }
     },
   },
