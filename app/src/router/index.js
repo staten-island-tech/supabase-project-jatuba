e5da9faa6e215169692b6f1815edb2f641aa468d
BatuhanCaglar <fbmanbc@gmail.com>
2025-05-28 13:40:36 -0400

    so

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -4,7 +4,6 @@ import SignUp from '../views/SignUp.vue'
 import UserLogin from '../views/UserLogin.vue'
 import UserAuth from '../views/UserAuth.vue'
 import PkmnPacksView from '@/views/PkmnPacksView.vue'
-import SetView from '@/views/SetView.vue'
 import MainView from '@/views/MainView.vue'
 import MainStore from '@/views/MainStore.vue'
 
@@ -15,7 +14,6 @@ const routes = [
   { path: '/login', component: UserLogin },
   { path: '/auth', component: UserAuth },
   { path: '/packs', name: 'Packs', component: PkmnPacksView },
-  { path: '/set', name: 'Sets', component: SetView },
   { path: '/main', component: MainView },
   { path: '/store', component: MainStore },
 ]
